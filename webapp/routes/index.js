b5a50a073cabf09053fc5c416bafb6f9ef397a80
BarBen2\Bengiamin <bengi.barblan@bluewin.ch>
2016-11-19 18:16:53 +0100

    blabla

--- webapp/routes/index.js
+++ webapp/routes/index.js
@@ -12,9 +12,23 @@ exports.register = function(server, options, next) {
     var io = require('socket.io')(server.listener);
 
     io.on('connection', function (socket) {
-
+        var audioBatch = [];
+        var count = 0;
         socket.on('audioData', function (payload) {
-            console.log('received' + JSON.stringify(payload));
+            //console.log('received' + JSON.stringify(payload));
+            audioBatch.concat(payload.audioBuffer.data);
+
+            count++;
+            if(count >= 100){
+                // process batch to API
+                console.log('100 blocks received');
+
+                //convert to WAV
+                var wav = require('node-wav');
+
+
+                count = 0;
+            }
 
         });
     });
