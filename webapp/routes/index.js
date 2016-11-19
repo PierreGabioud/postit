c0196bc81d5d27462e5183c77aac468d0fe1ff5d
BarBen2\Bengiamin <bengi.barblan@bluewin.ch>
2016-11-19 22:27:02 +0100

    no message

--- webapp/routes/index.js
+++ webapp/routes/index.js
@@ -10,22 +10,50 @@ exports.register = function(server, options, next) {
 
 
     var io = require('socket.io')(server.listener);
+    var fs = require('fs');
+
+
+    var profiles = [
+        '2876eb8f-37f4-4c53-8429-1364ffad4f45', //Bengi
+        'a499ce4c-6639-4940-82cc-3f9485e55370', //Marco
+        '26c07b8e-4537-4ff7-bc68-8686725942e8'  //Pierre
+    ]
 
     io.on('connection', function (socket) {
         var audioBatch = [];
         var count = 0;
+
         socket.on('audioData', function (payload) {
-            //console.log('received' + JSON.stringify(payload));
-            audioBatch.concat(payload.audioBuffer.data);
+
+            var test = JSON.stringify(payload.audioBuffer);
+            audioBatch = audioBatch + test;
+            console.log(test);
+
+            var wav = require('node-wav');
+            wav.encode(test, { sampleRate: 16000, float: true, bitDepth: 16 });
+            fs.writeFileSync('audio.wav', wav);
+
+
 
             count++;
             if(count >= 100){
                 // process batch to API
                 console.log('100 blocks received');
+                console.log(audioBatch);
 
                 //convert to WAV
                 var wav = require('node-wav');
+                wav.encode(audioBatch, { sampleRate: 16000, float: true, bitDepth: 16 });
+                fs.writeFileSync('audio.wav', wav);
 
+                //identify speaker on this block.
+
+                //identify(profiles, wav).then(function saveSpeaker(status){
+                  //  console.log(status);
+
+
+                 //   }
+                //);
 
                 count = 0;
             }
