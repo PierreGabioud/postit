3173f49d27c222eb4b81f8dff3e59cc3f70be8f6
Pierre Gabioud <pierregabioud@gmail.com>
2016-11-19 14:59:43 +0100

    nasty

---
+++ webapp/bingtest.js
@@ -0,0 +1,15 @@
+var bb = require('bingspeech-api-client');
+
+// audio input in a Buffer 
+var wav = fs.readFileSync('myaudiofile.wav');
+
+// Bing Speech Key (https://www.microsoft.com/cognitive-services/en-us/subscriptions) 
+var subscriptionKey = {
+    string : '4aacfbb33c6a4b8eb4573034275111f9'
+};
+
+var client = new bb.BingSpeechClient(subscriptionKey);
+client.recognize(wav)
+    .then(response => {
+    console.log(response.results[0].name);
+});
--- webapp/server.js
+++ webapp/server.js
@@ -1,3 +1,33 @@
-/**
- * Created by pierre on 19.11.16.
- */
+var request = require('request');
+
+var options = {
+    url: 'https://api.cognitive.microsoft.com/sts/v1.0/issueToken',
+    headers: {
+        'User-Agent': 'request',
+        'Ocp-Apim-Subscription-Key': '4aacfbb33c6a4b8eb4573034275111f9'
+    },
+    method:'POST'
+};
+
+function callback(error, response, body) {
+    var JWT = body;
+console.log(body);
+    console.log('Got JSON token');
+
+    request({
+       url: 'https://speech.platform.bing.com/recognize',
+        method: 'POST',
+        headers: {
+           'Authorization': 'Bearer '+JWT
+        }
+    }, function(error, response, body) {
+        //console.log(error);
+        //console.log(response);
+        console.log(body);
+    });
+
+
+}
+
+request(options, callback);
+
