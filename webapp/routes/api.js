c70f5fc19d252cdbc275f42ec97642c8f42ff86c
mgoretti <ht.davik@gmail.com>
2016-11-19 21:59:24 +0100

    clean

--- webapp/routes/api.js
+++ webapp/routes/api.js
@@ -1,39 +1,13 @@
 var db = require('../database');
 var _ = require('underscore');
-// db.storeBlockText(0, 0, "lalal");
-// db.storeBlockText(0, 1, "blabla");
+db.storeBlockText(0, 0, "lalal");
+db.storeBlockText(0, 1, "blabla");
 
-// db.storeBlockOwner(0, 0, "marco");
-// db.storeBlockOwner(0, 1, "pierre");
+db.storeBlockOwner(0, 0, "marco");
+db.storeBlockOwner(0, 1, "pierre");
 
 
 exports.register = function(server, options, next) {
-
-
-  var words = 'that is a wonderful idea I look forward to seeing you in hell you dumbass bullhock even summer is too much hell for you to see you bitch I like it you love it even though you go to the club';
-  var people = ['Pierre', 'Marco', 'BenJ'];
-  words = words.split(' ');
-
-
-  function getRandomSentence(){
-    function getRandomWord() {
-      return words[Math.floor(Math.random()*(words.length-1))];
-    }
-
-    var string = "";
-    for(var i=0; i<Math.random()*12; i++) {
-      string += getRandomWord()+' ';
-    }
-
-    return string;
-
-  }
-
-  function getRandomPerson() {
-    return people[Math.floor(Math.random()*people.length)];
-  }
-
-
   server.route({
     method:"GET",
     path:"/api/blockText/{expid}/{timeid?}",
