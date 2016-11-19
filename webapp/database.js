8abaf6e6e36d8c184c6807d405e6eecb2db117ca
mgoretti <ht.davik@gmail.com>
2016-11-19 21:58:27 +0100

    added db

--- webapp/database.js
+++ webapp/database.js
@@ -2,15 +2,47 @@
 
 const mongojs = require("mongojs");
 const db = mongojs("mongodb://localhost:27017/postit");
-const blocks = db.collection("blocks");
+const textBlocks = db.collection("textBlocks");
+const ownerBlocks = db.collection("ownerBlocks");
 
-exports.addBlock = function(blockData){
-    blocks.insert(blockData, function(err, result){
-       if(err) {
-           console.log(err);
-       }
-
-       console.log('Done adding block');
-    });
+exports.storeBlockText = function(experimentid, timeid, text) {
+  var blockData = {
+    experimentid: experimentid,
+    timeid: timeid,
+    text: text
+  }
+  textBlocks.insert(blockData, function(err, result){
+     if(err) {
+         console.log(err);
+     }
+  });
 };
+
+exports.storeBlockOwner = function(experimentid, timeid, owner) {
+  var blockData = {
+    experimentid: experimentid,
+    timeid: timeid,
+    owner: owner
+  }
+
+  ownerBlocks.insert(blockData, function(err, result){
+     if(err) {
+         console.log(err);
+     }
+  });
+};
+
+exports.getBlocksText = function(experimentid, timeid, cb) {
+  textBlocks.find({
+    experimentid: experimentid*1,
+    timeid: {"$gte": timeid*1}
+  }, cb);
+};
+
+exports.getBlocksOwner = function(experimentid, timeid, cb) {
+  ownerBlocks.find({
+    experimentid: experimentid*1,
+    timeid: {"$gte": timeid*1}
+  }, cb);
+}
 
--- webapp/routes/api.js
+++ webapp/routes/api.js
@@ -1,67 +1,68 @@
+var db = require('../database');
+var _ = require('underscore');
+// db.storeBlockText(0, 0, "lalal");
+// db.storeBlockText(0, 1, "blabla");
+
+// db.storeBlockOwner(0, 0, "marco");
+// db.storeBlockOwner(0, 1, "pierre");
+
+
 exports.register = function(server, options, next) {
 
 
-    var words = 'that is a wonderful idea I look forward to seeing you in hell you dumbass bullhock even summer is too much hell for you to see you bitch I like it you love it even though you go to the club';
-    var people = ['Pierre', 'Marco', 'BenJ'];
-    words = words.split(' ');
+  var words = 'that is a wonderful idea I look forward to seeing you in hell you dumbass bullhock even summer is too much hell for you to see you bitch I like it you love it even though you go to the club';
+  var people = ['Pierre', 'Marco', 'BenJ'];
+  words = words.split(' ');
 
 
-    function getRandomSentence(){
-        function getRandomWord() {
-            return words[Math.floor(Math.random()*(words.length-1))];
-        }
-
-        var string = "";
-        for(var i=0; i<Math.random()*12; i++) {
-            string += getRandomWord()+' ';
-        }
-
-        return string;
-
+  function getRandomSentence(){
+    function getRandomWord() {
+      return words[Math.floor(Math.random()*(words.length-1))];
     }
 
-    function getRandomPerson() {
-        return people[Math.floor(Math.random()*people.length)];
+    var string = "";
+    for(var i=0; i<Math.random()*12; i++) {
+      string += getRandomWord()+' ';
     }
 
+    return string;
 
-    server.route({
-        method:"GET",
-        path:"/api/blockContent",
-        handler: function(request, reply) {
+  }
 
-            var data = {
-                sentence : getRandomSentence(),
-                start: (new Date()).getTime() - 1000 * 5,
-                end: (new Date()).getTime()
-            };
-
-            reply(data);
-        },
-    });
+  function getRandomPerson() {
+    return people[Math.floor(Math.random()*people.length)];
+  }
 
 
-    server.route({
-        method:"GET",
-        path:"/api/blockPeople",
-        handler: function(request, reply) {
-
-            console.log(getRandomPerson());
-
-            var data = {
-                person : getRandomPerson(),
-                start: (new Date()).getTime() - 1000 * 5,
-                end: (new Date()).getTime()
-            };
-
-            reply(data);
-        },
-    });
+  server.route({
+    method:"GET",
+    path:"/api/blockText/{expid}/{timeid?}",
+    handler: function(request, reply) {
+      expid = encodeURIComponent(request.params.expid);
+      timeid = request.params.timeid ? encodeURIComponent(request.params.timeid) : 0;
+      db.getBlocksText(expid, timeid, function(err, blocks) {
+        reply(blocks);
+      });
+    },
+  });
 
 
-    return next();
+  server.route({
+    method:"GET",
+    path:"/api/blockOwner/{expid}/{timeid?}",
+    handler: function(request, reply) {
+      expid = encodeURIComponent(request.params.expid);
+      timeid = request.params.timeid ? encodeURIComponent(request.params.timeid) : 0;
+      db.getBlocksOwner(expid, timeid, function(err, blocks) {
+        reply(blocks);
+      });
+    },
+  });
+
+
+  return next();
 };
 
 exports.register.attributes = {
-    name: "route-api"
+  name: "route-api"
 };
